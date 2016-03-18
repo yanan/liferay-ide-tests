a9b978f66cae8341fadbbca1208989161b72dbb6
Terry Jia <terry.jia@liferay.com>
2016-03-18 10:55:04 +0800

    IDE-2312 project tests

---
+++ tools/com.liferay.ide.project.ui.tests/src/com/liferay/ide/project/ui/tests/ProjectWizardTests.java
@@ -0,0 +1,401 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.project.ui.tests;
+
+import static org.eclipse.swtbot.swt.finder.SWTBotAssert.assertContains;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.liferay.ide.project.ui.tests.page.CreateProjectWizardPO;
+import com.liferay.ide.project.ui.tests.page.ProjectTreePO;
+import com.liferay.ide.project.ui.tests.page.SelectPortletFrameworkPO;
+import com.liferay.ide.project.ui.tests.page.SetSDKLocationPO;
+import com.liferay.ide.project.ui.tests.page.ThemeWizardPO;
+import com.liferay.ide.ui.tests.SWTBotBase;
+import com.liferay.ide.ui.tests.swtbot.page.ButtonPO;
+import com.liferay.ide.ui.tests.swtbot.page.EditorPO;
+import com.liferay.ide.ui.tests.swtbot.page.ShellPO;
+import com.liferay.ide.ui.tests.swtbot.page.TextEditorPO;
+import com.liferay.ide.ui.tests.swtbot.page.TreePO;
+
+/**
+ * @author Terry Jia
+ * @author Ashley Yuan
+ * @author Vicky Wang
+ * @author Ying Xu
+ */
+public class ProjectWizardTests extends SWTBotBase implements ProjectWizard
+{
+
+    @After
+    public void waitForCreate()
+    {
+        sleep( 5000 );
+    }
+
+    @AfterClass
+    public static void cleanAll()
+    {
+        try
+        {
+            TreePO tree = new TreePO( bot );
+            String[] projects = tree.getAllItems();
+
+            for( String project : projects )
+            {
+                ProjectTreePO projectItem = new ProjectTreePO( bot, project );
+                projectItem.deleteProject();
+            }
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void createExtProject()
+    {
+        CreateProjectWizardPO page1 = new CreateProjectWizardPO( bot );
+        page1.createSDKProject( "text", MENU_EXT );
+
+        if( hasAddedProject )
+        {
+            page1.finish();
+        }
+        else
+        {
+            page1.next();
+
+            SetSDKLocationPO page2 = new SetSDKLocationPO( bot );
+            page2.setSdkLocation( getLiferayPluginsSdkDir().toString() );
+            page2.finish();
+        }
+    }
+
+    @Test
+    public void createHookProject()
+    {
+        CreateProjectWizardPO page1 = new CreateProjectWizardPO( bot );
+        page1.createSDKProject( "text", MENU_HOOK );
+
+        if( hasAddedProject )
+        {
+            page1.finish();
+        }
+        else
+        {
+            page1.next();
+
+            SetSDKLocationPO page2 = new SetSDKLocationPO( bot );
+            page2.setSdkLocation( getLiferayPluginsSdkDir().toString() );
+            page2.finish();
+        }
+
+        sleep(5000);
+
+        eclipse.showErrorLogView().clearLogViewer();
+    }
+
+    @Test
+    public void createServiceBuilderPortletProject()
+    {
+        CreateProjectWizardPO page1 = new CreateProjectWizardPO( bot );
+        page1.createSDKProject( "textsb", MENU_SERVICE_BUILDER_PORTLET, true );
+
+        if( hasAddedProject )
+        {
+            page1.finish();
+
+            page1.sleep();
+        }
+        else
+        {
+            page1.next();
+
+            SetSDKLocationPO page2 = new SetSDKLocationPO( bot );
+            page2.setSdkLocation( getLiferayPluginsSdkDir().toString() );
+            page2.finish();
+        }
+    }
+
+    @Test
+    public void createServiceBuilderPortletProjectWithoutSampleCode()
+    {
+        CreateProjectWizardPO page1 = new CreateProjectWizardPO( bot );
+
+        page1.createSDKProject( "textsbwithoutcode", MENU_SERVICE_BUILDER_PORTLET, false );
+
+        if( hasAddedProject )
+        {
+            page1.finish();
+        }
+        else
+        {
+            page1.next();
+
+            SetSDKLocationPO page2 = new SetSDKLocationPO( bot );
+            page2.setSdkLocation( getLiferayPluginsSdkDir().toString() );
+            page2.finish();
+        }
+    }
+
+    @Test
+    public void createLayoutProject()
+    {
+        CreateProjectWizardPO page1 = new CreateProjectWizardPO( bot );
+
+        page1.createSDKProject( "text", MENU_LAYOUT_TEMPLATE );
+
+        if( hasAddedProject )
+        {
+            page1.finish();
+        }
+        else
+        {
+            page1.next();
+
+            SetSDKLocationPO page2 = new SetSDKLocationPO( bot );
+            page2.setSdkLocation( getLiferayPluginsSdkDir().toString() );
+            page2.finish();
+        }
+    }
+
+    @Test
+    public void createPortletProject()
+    {
+        String projectName = "testPortlet";
+
+        CreateProjectWizardPO page1 =
+            new CreateProjectWizardPO( bot, TOOLTIP_MENU_ITEM_NEW_LIFERAY_PROJECT );
+
+        page1.createSDKProject( projectName, MENU_PORTLET, true, false );
+
+        assertTrue( page1.IsIncludeSimpleCodeCheckBoxChecked() );
+        assertFalse( page1.IsLaunchNewPortletWizardCheckBoxChecked() );
+
+        page1.next();
+
+        SelectPortletFrameworkPO page2 = new SelectPortletFrameworkPO( bot );
+
+        assertTrue( page2.IsLiferayMVCRadioSelected() );
+
+        if( !hasAddedProject )
+        {
+            page1.next();
+
+            SetSDKLocationPO page3 = new SetSDKLocationPO( bot );
+
+            page3.setSdkLocation( getLiferayPluginsSdkDir().toString() );
+        }
+
+        page1.finish();
+
+        // check in console and package explorer
+        // assertTrue( UITestsUtils.checkConsoleMessage( "BUILD SUCCESSFUL", "Java" ) );
+
+        //treeBot.doubleClick( "liferay-display.xml", projectName + "-portlet", "docroot", "WEB-INF" );
+
+        EditorPO editorPage = new EditorPO( bot, "liferay-display.xml" );
+        assertTrue( editorPage.isActive() );
+
+        TextEditorPO textEditorPage = new TextEditorPO( bot, "liferay-display.xml" );
+        assertContains( "sample", textEditorPage.getText() );
+
+        eclipse.getCreateLiferayProjectToolbar().menuClick( TOOLTIP_MENU_ITEM_NEW_LIFERAY_PROJECT );
+
+        CreateProjectWizardPO page4 =
+            new CreateProjectWizardPO( bot, INDEX_VALIDATION_MESSAGE3 );
+
+        page4.createSDKPortletProject( projectName );
+        assertEquals( TEXT_PROJECT_ALREADY_EXISTS, page4.getValidationMessage() );
+
+        page4.createSDKPortletProject( projectName + "-portlet" );
+        assertEquals( TEXT_PROJECT_ALREADY_EXISTS, page4.getValidationMessage() );
+
+        page4.cancel();
+
+        eclipse.getCreateLiferayProjectToolbar().menuClick( TOOLTIP_MENU_ITEM_NEW_LIFERAY_PROJECT );
+
+        CreateProjectWizardPO page5 = new CreateProjectWizardPO( bot, INDEX_VALIDATION_MESSAGE3 );
+
+        page5.createSDKPortletProject( projectName );
+        assertContains( projectName + "-portlet\"" + TEXT_PROJECT_EXISTS_IN_LOCATION, page5.getValidationMessage() );
+
+        page5.createSDKPortletProject( projectName + "-portlet" );
+        assertContains( projectName + "-portlet\"" + TEXT_PROJECT_EXISTS_IN_LOCATION, page5.getValidationMessage() );
+
+        page5.cancel();
+
+        deleteProjectInSdk( projectName + "-portlet", getLiferayPluginsSdkName(), "portlets" );
+    }
+
+    @Test
+    public void createPortletProjectWithoutSampleAndLaunchNewPortletWizard()
+    {
+        CreateProjectWizardPO page1 = new CreateProjectWizardPO( bot );
+
+        page1.createSDKProject( "NoSampleTest", MENU_PORTLET, false, true );
+
+        assertFalse( page1.IsIncludeSimpleCodeCheckBoxChecked() );
+        assertTrue( page1.IsLaunchNewPortletWizardCheckBoxChecked() );
+
+        page1.next();
+
+        SelectPortletFrameworkPO page2 = new SelectPortletFrameworkPO( bot );
+
+        assertTrue( page2.IsLiferayMVCRadioSelected() );
+
+        if( !hasAddedProject )
+        {
+            page1.next();
+
+            SetSDKLocationPO page3 = new SetSDKLocationPO( bot );
+
+            page3.setSdkLocation( getLiferayPluginsSdkDir().toString() );
+        }
+
+        page1.finish();
+
+        ShellPO newPortletPage = new ShellPO( bot, "New Liferay Portlet" ){};
+
+        newPortletPage.waitForPageToOpen();
+
+        assertEquals( TOOLTIP_NEW_LIFERAY_PORTLET, newPortletPage.getTitle() );
+
+        newPortletPage.closeIfOpen();
+    }
+
+    @Test
+    public void createThemeProject()
+    {
+        CreateProjectWizardPO page1 = new CreateProjectWizardPO( bot );
+        ThemeWizardPO page2 = new ThemeWizardPO( bot, INDEX_VALIDATION_MESSAGE3 );
+
+        String defaultMessage = "Select options for creating new theme project.";
+        String warningMessage = " For advanced theme developers only.";
+
+        String projectThemeName = "test";
+
+        page1.createSDKProject( projectThemeName, MENU_THEME );
+
+        page1.next();
+
+        //assertEquals( defaultMessage, page2.getValidationMessage() );
+
+        page2.setParentFramework( MENU_THEME_PARENT_UNSTYLED, MENU_THEME_FRAMEWORK_JSP );
+        assertEquals( warningMessage, page2.getValidationMessage() );
+
+        page2.setParentFramework( MENU_THEME_PARENT_CLASSIC, MENU_THEME_FRAMEWORK_VELOCITY );
+        assertEquals( defaultMessage, page2.getValidationMessage() );
+
+        page2.setParentFramework( MENU_THEME_PARENT_STYLED, MENU_THEME_FRAMEWORK_FREEMARKER );
+
+        if( hasAddedProject )
+        {
+            page2.finish();
+        }
+        else
+        {
+            page2.next();
+
+            SetSDKLocationPO page3 = getSetSDKLoactionPage();
+
+            page3.finish();
+        }
+
+        sleep( 15000 );
+
+//        TreeItemPO buildXml = new TreeItemPO( bot, projectThemeName + "-theme", "build.xml" );
+//        buildXml.doubleClick();
+
+        ProjectTreePO project = new ProjectTreePO( bot, projectThemeName + "-theme" );
+
+        project.deleteProject();
+    }
+
+    @Test
+    public void createWebProject()
+    {
+        CreateProjectWizardPO page1 = new CreateProjectWizardPO( bot, INDEX_VALIDATION_MESSAGE3 );
+
+        page1.createSDKProject( "text", MENU_WEB );
+
+        if( hasAddedProject )
+        {
+            sleep( 1500 );
+            assertEquals( TEXT_WEB_SDK_62_ERRORR_MESSAGE, page1.getValidationMessage() );
+            page1.cancel();
+        }
+        else
+        {
+            page1.next();
+            SetSDKLocationPO page2 = new SetSDKLocationPO( bot, INDEX_VALIDATION_MESSAGE2 );
+
+            page2.setSdkLocation( getLiferayPluginsSdkDir().toString() );
+
+            sleep( 1500 );
+            assertEquals( TEXT_WEB_SDK_62_ERRORR_MESSAGE, page2.getValidationMessage() );
+            page2.cancel();
+        }
+    }
+
+    @Test
+    public void validationProjectName()
+    {
+        String invalidNameDoubleDash = "--";
+
+        CreateProjectWizardPO page1 =
+            new CreateProjectWizardPO( bot, INDEX_VALIDATION_MESSAGE3 );
+
+        page1.createSDKPortletProject( invalidNameDoubleDash );
+
+        sleep( 1500 );
+        assertEquals( " The project name is invalid.", page1.getValidationMessage() );
+
+        page1.cancel();
+    }
+
+    public static void deleteProjectInSdk( String projectName, String... nodes )
+    {
+        //treeBot.expandNode( nodes ).getNode( projectName ).contextMenu( BUTTON_DELETE ).click();
+
+        new ButtonPO( bot, BUTTON_OK ).click();
+    }
+
+    private SetSDKLocationPO getSetSDKLoactionPage()
+    {
+        SetSDKLocationPO page = new SetSDKLocationPO( bot );
+        page.setSdkLocation( getLiferayPluginsSdkDir().toString() );
+
+        return page;
+    }
+
+    @Before
+    public void openWizard()
+    {
+        hasAddedProject = addedProjects();
+
+        eclipse.getCreateLiferayProjectToolbar().menuClick( TOOLTIP_MENU_ITEM_NEW_LIFERAY_PROJECT );
+    }
+
+}
---
+++ tools/com.liferay.ide.project.ui.tests/src/com/liferay/ide/project/ui/tests/page/CreateProjectWizardPO.java
@@ -0,0 +1,173 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.project.ui.tests.page;
+
+import com.liferay.ide.project.ui.tests.ProjectWizard;
+import com.liferay.ide.ui.tests.swtbot.page.CheckBoxPO;
+import com.liferay.ide.ui.tests.swtbot.page.ComboBoxPO;
+import com.liferay.ide.ui.tests.swtbot.page.TextPO;
+import com.liferay.ide.ui.tests.swtbot.page.WizardPO;
+
+import org.eclipse.swtbot.swt.finder.SWTBot;
+
+/**
+ * @author Terry Jia
+ * @author Ashley Yuan
+ */
+public class CreateProjectWizardPO extends WizardPO implements ProjectWizard
+{
+
+    private ComboBoxPO _buildTypeComboBox;
+    private TextPO _displayNameText;
+    private CheckBoxPO _includeSimpleCodeCheckBox;
+    private CheckBoxPO _launchNewPortletWizardCheck;
+    private ComboBoxPO _pluginTypeComboBox;
+    private TextPO _projectNameText;
+
+    public CreateProjectWizardPO( SWTBot bot )
+    {
+        this( bot, INDEX_DEFAULT_VALIDATION_MESSAGE );
+    }
+
+    public CreateProjectWizardPO( SWTBot bot, int index )
+    {
+        this( bot, TEXT_BLANK, index );
+    }
+
+    public CreateProjectWizardPO( SWTBot bot, String title )
+    {
+        this( bot, title, INDEX_DEFAULT_VALIDATION_MESSAGE );
+    }
+
+    public CreateProjectWizardPO( SWTBot bot, String title, int validationMessageIndex )
+    {
+        super( bot, title, BUTTON_CANCEL, BUTTON_FINISH, BUTTON_BACK, BUTTON_NEXT, validationMessageIndex );
+
+        _projectNameText = new TextPO( bot, LABEL_PROJECT_NAME );
+        _displayNameText = new TextPO( bot, LABEL_DISPLAY_NAME );
+        _buildTypeComboBox = new ComboBoxPO( bot, LABEL_BUILD_TYPE );
+        _pluginTypeComboBox = new ComboBoxPO( bot, LABEL_PLUGIN_TYPE );
+        _includeSimpleCodeCheckBox = new CheckBoxPO( bot, LABEL_INCLUDE_SAMPLE_CODE );
+        _launchNewPortletWizardCheck = new CheckBoxPO( bot, LABEL_LAUNCH_NEW_PORTLET_WIZARD_AFTER_PROJECT );
+    }
+
+    public void createSDKPortletProject( String projectName )
+    {
+        createSDKProject( projectName, MENU_PORTLET );
+    }
+
+    public void createSDKProject( String projectName, String pluginType )
+    {
+        createSDKProject( projectName, TEXT_BLANK, pluginType );
+    }
+
+    public void createSDKProject( String projectName, String pluginType, boolean includeSimpleCode )
+    {
+        createSDKProject( projectName, pluginType, includeSimpleCode, false );
+    }
+
+    public void createSDKProject(
+        String projectName, String pluginType, boolean includeSimpleCode, boolean launchNewPortletWizard )
+    {
+        createSDKProject( projectName, TEXT_BLANK, pluginType, includeSimpleCode, launchNewPortletWizard );
+    }
+
+    public void createSDKProject( String projectName, String displayName, String pluginType )
+    {
+        createSDKProject( projectName, displayName, pluginType, false, false );
+    }
+
+    public void createSDKProject(
+        String projectName, String displayName, String pluginType, boolean includeSimpleCode,
+        boolean launchNewPortletWizard )
+    {
+        _projectNameText.setText( projectName );
+
+        if( displayName != null && !displayName.equals( TEXT_BLANK ) )
+        {
+            _displayNameText.setText( displayName );
+        }
+
+        _buildTypeComboBox.setSelection( MENU_BUILD_TYPE_ANT );
+
+        _pluginTypeComboBox.setSelection( pluginType );
+
+        if( pluginType.equals( MENU_PORTLET ) || pluginType.equals( MENU_SERVICE_BUILDER_PORTLET ) )
+        {
+            if( includeSimpleCode )
+            {
+                _includeSimpleCodeCheckBox.select();
+            }
+            else
+            {
+                _includeSimpleCodeCheckBox.deselect();
+            }
+        }
+
+        if( pluginType.equals( MENU_PORTLET ) )
+        {
+            if( launchNewPortletWizard )
+            {
+                _launchNewPortletWizardCheck.select();
+            }
+            else
+            {
+                _launchNewPortletWizardCheck.deselect();
+            }
+        }
+    }
+
+    public String getBuildTypeComboBox()
+    {
+        return _buildTypeComboBox.getText();
+    }
+
+    public String getPluginTypeComboBox()
+    {
+        return _pluginTypeComboBox.getText();
+    }
+
+    public String getProjectNameText()
+    {
+        return _projectNameText.getText();
+    }
+
+    public boolean IsIncludeSimpleCodeCheckBoxChecked()
+    {
+        return _includeSimpleCodeCheckBox.isChecked();
+    }
+
+    public boolean IsLaunchNewPortletWizardCheckBoxChecked()
+    {
+        return _launchNewPortletWizardCheck.isChecked();
+    }
+
+    public void setBuildTypeComboBox( ComboBoxPO buildTypeComboBox )
+    {
+        this._buildTypeComboBox = buildTypeComboBox;
+    }
+
+    public void setPluginTypeComboBox( ComboBoxPO pluginTypeComboBox )
+    {
+        this._pluginTypeComboBox = pluginTypeComboBox;
+    }
+
+    public void setProjectName( TextPO projectName )
+    {
+        _projectNameText = projectName;
+    }
+
+}
